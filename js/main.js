cd9f501a5202c3850931c250ee05e6069dfd424c
Ekaterina Diatchenko <t1ad1at@gmail.com>
2018-06-20 20:19:22 -0700

    completed

--- js/main.js
+++ js/main.js
@@ -11,14 +11,56 @@ function greet() {
 }
 
 
-var myArray = ["We", "learn", "code", "on", "Mondays"];
+var myArray = ["We", "learn", "javascript", "on", "Mondays"];
 
 for(var i=0; i<myArray.length; i++) {
 	console.log(myArray[i]);
 }
 
 
-	
+var myDog = {
+	name: "Zorie",
+	age: 3,
+	isSpoiled: true
+};
+
+console.log(myDog.name); 
+console.log(myDog.age);
+console.log(myDog.isSpoiled);
+
+var three = document.getElementById("three");
+var four = document.getElementById("four");
+var five = document.getElementById("five");
+
+	three.innerText = myDog.name;
+	four.innerText = myDog.age;
+	five.innerText = myDog.isSpoiled;
+
+var newArray = [
+		{
+			movie: "Ocean's 8",
+			genre: "Crime film/Action",
+			rating: "66% on rotten tomatoes"
+		},
+		{
+			movie: "Superfly",
+			genre: "Action, Crime, Thriller",
+			rating: "52% on rotten tomatoes"
+		},
+		{
+			movie: "Blockers",
+			genre: "Comedy",
+			rating: "83% on rotten tomatoes"
+		}
+]; 
+
+for (i=0; i<newArray.length; i++) {
+	console.log(newArray[i].movie);
+	console.log(newArray[i].genre);
+	console.log(newArray[i].rating);
+}
+
+document.body.style.color = "red";
 
 
  	
